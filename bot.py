8e5a9d574feac3b098ad386c2f5050d978486198
NikitosMetla <126060075+NikitosMetla@users.noreply.github.com>
2024-03-24 00:43:18 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,15 +7,15 @@ from handlers.mentor_handler import mentor_router
 from handlers.send_link_handler import send_link_router
 from settings import storage_bot, token_design_level
 
-bot = Bot(token=token_design_level, parse_mode="html")
+design_bot = Bot(token=token_design_level, parse_mode="html")
 
 
 async def main():
-    print(await bot.get_me())
-    await bot.delete_webhook(drop_pending_updates=True)
+    print(await design_bot.get_me())
+    await design_bot.delete_webhook(drop_pending_updates=True)
     dp = Dispatcher(storage=storage_bot)
     dp.include_routers(mentor_router, design_level_router, earnings_level_router, send_link_router)
-    await dp.start_polling(bot)
+    await dp.start_polling(design_bot)
 
 
 if __name__ == "__main__":
